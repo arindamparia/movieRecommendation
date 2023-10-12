ecd84d4b07a00cd6780878d7b0619c90d67da50a
arindamparia <arindamparia321@gmail.com>
2023-10-12 22:22:44 +0530

    Adding some more movie related details

--- index.js
+++ index.js
@@ -1,57 +1,72 @@
-const genresEle=document.getElementById('genres'),searchBtnEle=document.getElementById('playBtn')
-const genreRequest=new XMLHttpRequest()
-const apiKey='5d3798c7cb18ed0ae769fc0e4e5fd90f'
-genreRequest.open('GET',`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
+const genresEle = document.getElementById('genres'), searchBtnEle = document.getElementById('playBtn')
+const genreRequest = new XMLHttpRequest()
+const apiKey = '5d3798c7cb18ed0ae769fc0e4e5fd90f'
+genreRequest.open('GET', `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
 genreRequest.send()
-genreRequest.onload=()=> {
-    const genreList=JSON.parse(genreRequest.responseText)
-    genreList.genres.forEach(val=>{
-        const opt=document.createElement('option')
-        opt.textContent=val.name;
+genreRequest.onload = () => {
+    const genreList = JSON.parse(genreRequest.responseText)
+    genreList.genres.forEach(val => {
+        const opt = document.createElement('option')
+        opt.textContent = val.name;
         genresEle.append(opt)
     })
-    searchBtnEle.onclick=()=>{
-        const selectedGenre=document.getElementById('genres').value
-        const selectedGenreID=genreList.genres.find(val=>val.name===selectedGenre).id
-        const movieRequest=new XMLHttpRequest()
-        movieRequest.open('GET',`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenreID}`)
+    searchBtnEle.onclick = () => {
+        const selectedGenre = document.getElementById('genres').value
+        const selectedGenreID = genreList.genres.find(val => val.name === selectedGenre).id
+        const movieRequest = new XMLHttpRequest()
+        movieRequest.open('GET', `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenreID}`)
         movieRequest.send()
-        movieRequest.onload=movieLoad
-        function movieLoad(){
-            const movieListPageCnt=JSON.parse(movieRequest.responseText).total_pages;
-            const movieListPage=Math.floor(Math.random()*Math.min(movieListPageCnt,500))+1
-            const movieRequestWithPage=new XMLHttpRequest()
-            movieRequestWithPage.open('GET',`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenreID}&page=${movieListPage}`)
+        movieRequest.onload = movieLoad
+
+        function movieLoad() {
+            const movieListPageCnt = JSON.parse(movieRequest.responseText).total_pages;
+            const movieListPage = Math.floor(Math.random() * Math.min(movieListPageCnt, 500)) + 1
+            const movieRequestWithPage = new XMLHttpRequest()
+            movieRequestWithPage.open('GET', `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenreID}&page=${movieListPage}`)
             movieRequestWithPage.send()
-            movieRequestWithPage.onload=()=>{
-                const movieList=JSON.parse(movieRequestWithPage.responseText).results
-                const movieID=movieList[Math.floor(Math.random()*movieList.length)].id
-                const movieDetailsRequest=new XMLHttpRequest()
-                movieDetailsRequest.open('GET',`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}`)
+            movieRequestWithPage.onload = () => {
+                const movieList = JSON.parse(movieRequestWithPage.responseText).results
+                const movieID = movieList[Math.floor(Math.random() * movieList.length)].id
+                const movieDetailsRequest = new XMLHttpRequest()
+                movieDetailsRequest.open('GET', `https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}`)
                 movieDetailsRequest.send()
-                movieDetailsRequest.onload=()=>{
-                    const movieDetails=JSON.parse(movieDetailsRequest.responseText)
-                    if(movieDetails.poster_path) {
+                movieDetailsRequest.onload = () => {
+                    const movieDetails = JSON.parse(movieDetailsRequest.responseText)
+                    console.log(movieDetails)
+                    const moviePostEle = document.getElementById('moviePoster')
+                    if (movieDetails.poster_path) {
                         const moviePosterImgEle = document.createElement('img')
                         moviePosterImgEle.src = `https://image.tmdb.org/t/p/original/${movieDetails.poster_path}`
-                        const moviePostEle = document.getElementById('moviePoster')
                         moviePostEle.innerHTML = ''
                         moviePostEle.append(moviePosterImgEle)
-                    }
-                    else{
+                    } else {
                         moviePostEle.textContent = `Image not found`
                     }
-                    const movieTitleEle=document.createElement('div')
-                    movieTitleEle.id='movieTitle';
-                    movieTitleEle.textContent=movieDetails.title
-                    const movieOverviewEle=document.createElement('div')
-                    movieOverviewEle.id='movieOverview';
-                    movieOverviewEle.textContent=movieDetails.overview
-                    const movieTextEle=document.getElementById('movieText')
-                    movieTextEle.innerHTML=''
-                    movieTextEle.append(movieTitleEle,movieOverviewEle)
-                    document.getElementById('likeOrDislikeBtns').hidden=false
-                    document.getElementById('likeBtn').onclick=movieLoad
+                    const movieTextEle = document.getElementById('movieText')
+                    movieTextEle.innerHTML = ''
+                    const movieTitleEle = document.createElement('div')
+                    movieTitleEle.id = 'movieTitle';
+                    movieTitleEle.textContent = movieDetails.title
+                    movieTextEle.append(movieTitleEle)
+                    if (movieDetails.release_date) {
+                        const movieReleaseDateEle = document.createElement('div')
+                        movieReleaseDateEle.textContent = `Release Date- ${movieDetails.release_date}`
+                        movieReleaseDateEle.classList.add('movieRelease')
+                        movieTextEle.append(movieReleaseDateEle)
+                    }
+                    if (movieDetails.runtime) {
+                        const movieRuntimeEle = document.createElement('div')
+                        movieRuntimeEle.textContent = `Duration- ${movieDetails.runtime} minutes`
+                        movieRuntimeEle.classList.add('movieDuration')
+                        movieTextEle.append(movieRuntimeEle)
+                    }
+                    const movieOverviewEle = document.createElement('div')
+                    movieOverviewEle.id = 'movieOverview';
+                    movieOverviewEle.textContent = movieDetails.overview
+
+                    movieTextEle.append(movieOverviewEle)
+                    document.getElementById('likeOrDislikeBtns').hidden = false
+                    document.getElementById('likeBtn').onclick = movieLoad
                 }
             }
         }
